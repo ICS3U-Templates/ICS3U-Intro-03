775c0601b3d9ae9917fe163b6150d1073e8cd539
Patrick Coxall <mr.coxall@mths.ca>
2023-01-08 20:18:46 -0500

    updated test

--- .devcontainer/initial_startup.sh
+++ .devcontainer/initial_startup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y install --no-install-recommends build-essential gdb
-sudo pip3 install cpplint
-
 # now update the README.md file
 GITHUB_URL=$(git remote get-url origin)
 #GITHUB_USER=$(git config user.name)
@@ -25,13 +20,14 @@ fi
 FIRST_CHAR=$(head -c 1 ./README.md)
 if [ "$FIRST_CHAR" = "[" ]
 then
+    cp ./.devcontainer/.bashrc ~/.bashrc
     ex -s -c 1m3 -c w -c q ./README.md
     sed -i "3G" ./README.md
     sed -i "s/<OWNER>/$GITHUB_REPO_OWNER/g" ./README.md
     sed -i "s/<REPOSITORY>/$GITHUB_REPO_NAME/g" ./README.md
-    cp ./.devcontainer/.bashrc ~/.bashrc
     # shellcheck disable=SC1090
     #source ~/.bashrc
+    exec bash
     git add -A
     git commit -m "updated the badges"
     git push origin main
@@ -41,5 +37,4 @@ then
     echo "Already changed."
 else
     echo "Not what it should be!"
-    exit 1
 fi
